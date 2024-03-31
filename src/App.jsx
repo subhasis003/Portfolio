dc04c52c5aef6eb839d3c2fe267beec6e4cc0fe3
Subhasis Majee <subhasis.majee03@gmail.com>
2024-03-31 19:54:24 +0530

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { StyledComponent } from "@emotion/styled";
+function App() {
+
+    return (
+        <>
+            <p>Portfolio</p>
+        </>
+    );
+}
+
+export default App;
